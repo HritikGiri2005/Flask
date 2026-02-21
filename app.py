d60e7fce9dbb01400f379e78f42bf988c9c9a3d6
HritikGiri2005 <hritikgiri2@gmail.com>
2026-02-21 12:26:21 +0530

    rendered hello template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 #create route
@@ -9,6 +9,10 @@ def hello_world():
 @app.route('/test')
 def test_app():
     return '<p> test route </p>'
+
+@app.route('/temp')
+def temp_view():
+    return render_template("hello.html")
 
 if __name__ == '__main__':
     app.run()
