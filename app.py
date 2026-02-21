e1403b32b75fb818210b865c928e30ec75b1702c
HritikGiri2005 <hritikgiri2@gmail.com>
2026-02-21 13:01:33 +0530

    created User model in app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(150), nullable = False)
+
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+    user = User(name = "Sachin",email = "sachin@email.com")
+    db.session.add(User)
+    db.session.commit()
 #create route
 @app.route('/')
 def hello_world():
